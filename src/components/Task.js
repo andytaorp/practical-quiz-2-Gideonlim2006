03aa00873c4bf04d7422b844565541ff821b103e
Gideonlim2006 <23023165@myrp.edu.sg>
2024-11-27 20:18:57 +0800

    dwddwdwsa

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,20 +6,29 @@ import React from "react";
  * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
  */
 export default function Task({ task, onToggleTask, onDeleteTask }) {
+  let textDecorationStyle;
+  
+  if (task.completed) {
+    textDecorationStyle = "line-through";
+  } else {
+    textDecorationStyle = "none";
+  }
+
   return (
-    <li style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+    <li style={{ textDecoration: textDecorationStyle }}>
       <div>
         <input
           type="checkbox"
           checked={task.completed}
-          onChange={() => onToggleTask(task.id)} 
+          onChange={() => onToggleTask(task.id)}
         />
         {task.description}
-        <button onClick={() => onDeleteTask(task.id)}>Delete</button> 
+        <button onClick={() => onDeleteTask(task.id)}>Delete</button>
       </div>
     </li>
   );
 }
+
 
 
 
